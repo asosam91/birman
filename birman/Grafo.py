63826f6f7303a4e9cb7b92b26d10976a4078d87c
Adrian Sosa <asosam91@gmail.com>
2019-03-08 15:23:30 -0600

    create birman directory

---
+++ birman/Grafo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+class Grafo:
+	def __init__(self, proceso):
+		self.__proceso = proceso
+		self.__procesos = np.zeros((proceso,proceso))
+
+	def getProceso(self):
+		return self.__proceso
+
+ 	def update(self, mensaje):
+
+
+	def __str__(self):
+		return self.__procesos
+
