dbeec52314a93e61227c745c740bb4e18b707eaa
Adriano Sokax <asosam91gmail.com>
2019-03-10 14:04:07 -0600

    Mensaje crated, Mensaje class created

--- birman/Mensaje.py
+++ birman/Mensaje.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import numpy as np
 class Mensaje:
-	def __init__(self):
-		pass
+	def __init__(self, proceso,procesos):
+		self.__mensaje = np.zeros((procesos))
+		self.__proceso = proceso
+
+	def setMensaje(self, mensaje):
+		for p in range(len(mensaje)):
+			if self.__mensaje[p] != mensaje[p]:
+				self.__mensaje[p] = mensaje[p] 
+
+	def getMensaje(self):
+		return self.__mensaje
+
+	def getProceso(self):
+		return self.__proceso
+
+	def __str__(self):
+		self.__Mensaje= []
+		self.__Mensaje.append(self.__proceso)
+		self.__Mensaje.append(self.__mensaje)
+		return self.__Mensaje
