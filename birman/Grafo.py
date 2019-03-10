a0083ac1925c890587893622087c6ab32ecdfd38
Adriano Sokax <asosam91gmail.com>
2019-03-10 13:47:15 -0600

    grafo update, setProceso method defined

--- birman/Grafo.py
+++ birman/Grafo.py
@@ -2,17 +2,21 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 class Grafo:
-	def __init__(self, proceso):
-		self.__proceso = proceso
-		self.__procesos = np.zeros((proceso,proceso))
+	def __init__(self, procesos):
+		self.__procesos = procesos
+		self.__proceso = np.zeros((procesos,procesos))
 
-	def getProceso(self):
-		return self.__proceso
+	def getProcesos(self):
+		return self.__procesos
 
- 	def update(self, mensaje):
- 		pass
-
+	def setProceso(self, mensaje):
+		for p in range(len(mensaje)):
+			if self.__proceso[p] < mensaje[p]:
+				self.__proceso[p] = mensaje[p]  
 
 	def __str__(self):
-		return self.__procesos
+		return self.__proceso
+
+	def getProceso(self, num):
+		return self.__proceso[num]
 
